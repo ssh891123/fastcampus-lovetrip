0c53f78d2becd12ba30f27c88ad6d955e70eddab
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-10-14 16:41:23 +0900

    [feat]프로젝트 생성과 환경 설정
    1. 프로젝트 생성과 환경설정
    - 패키징 관리를 위한 yarn berry 설정
      - 무겁고 복잡한 node modules(프로젝트가 거대해질수록 무거워짐)대신 yarn berry 사용
      - yarn berry에 pnp 환경으로 세팅을 위한 yarn install
    - pnp 방식으로 패키징 설정을 위한 패키징 설정
      - node_modules를 설정하지 않으므로, node-linker를 pnp로 설정(yarnrc.yaml)
    - eslint, prettier 설정
    - import  설정시 alias를 이용하기 위한 craco 설정
    
    2. mcard 프로젝트의 일부 컴포너트 가져옴
    - components/shared 폴더의 tsx파일(MyImage는 제외)
    - styles폴더의 ts 파일
    
    3. 글로벌 스타일 설정
    - 최상위(root)단에 글로벌 스타일 설정(index.tsx)
    
    4. firebase 설정
    - back단의 설정을 위한 firebase 사용(app, auth, storage)

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,56 @@
+import { useCallback } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { css } from '@emotion/react'
+
+import Flex from '@shared/Flex'
+import Button from '@shared/Button'
+// import MyImage from '@shared/MyImage'
+import { colors } from '@styles/colorPalette'
+
+function Navbar() {
+  const location = useLocation()
+  const showSignButton =
+    ['/signin', '/signup'].includes(location.pathname) === false
+  // @TODO
+  const user = null
+
+  const renderButton = useCallback(() => {
+    if (user != null) {
+      //TODO 클릭시 user 정보 페이지로 이동 기능 추가
+      return (
+        <Link to="/my">
+          {/* @TODO */}
+          <img src="" alt="" />
+        </Link>
+      )
+    }
+
+    if (showSignButton) {
+      return (
+        <Link to="/signin">
+          <Button>회원가입/로그인</Button>
+        </Link>
+      )
+    }
+
+    return null
+  }, [user, showSignButton])
+
+  return (
+    <Flex justify="space-between" align="center" css={navbarContainer}>
+      <Link to="/">홈</Link>
+      {renderButton()}
+    </Flex>
+  )
+}
+
+const navbarContainer = css`
+  padding: 10px 24px;
+  top: 0;
+  position: sticky;
+  background-color: ${colors.white};
+  z-index: 10;
+  border-bottom: 1px solid;
+`
+
+export default Navbar
