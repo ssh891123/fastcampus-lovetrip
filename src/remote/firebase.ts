562d5f9ea9ec81ea9aa58af255fb4f74c11f7673
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-10-15 15:24:27 +0900

    [feat] 데이터 준비
    - test 페이지에서 mock 데이터를 firestore에 보관

--- src/remote/firebase.ts
+++ src/remote/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
 
 const {
   REACT_APP_API_KEY,
@@ -24,4 +24,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
-export const storage = getStorage(app)
+export const store = getFirestore(app)
