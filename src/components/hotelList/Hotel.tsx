9f4a472a574f8439f343d0e1635c897727fb6b4c
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-10-18 14:13:11 +0900

    [feat] 호텔 이벤트 추가
    - 호텔의 이벤트 정보를 tag(컴포넌트)로 달리도록 기능 추가
      - tag component 구현
      - 핫딜 이벤트 UX 구현
    
    - 핫딜 이벤트 실시간 정보 기능 구현
      - setInterval로 1초마다 남은시간 계산
      - 남은시간 계산은 formatTime 유틸함수, data-fns 라이브러리 사용
      - useState로 state 업데이트 및 리렌더링

--- src/components/hotelList/Hotel.tsx
+++ src/components/hotelList/Hotel.tsx
@@ -1,38 +1,100 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { css } from '@emotion/react'
+import { differenceInMilliseconds, parseISO } from 'date-fns'
 import { Hotel as IHotel } from '@models/hotel'
 import Flex from '@shared/Flex'
 import ListRow from '@shared/ListRow'
+import Tag from '@shared/Tag'
 import Spacing from '@shared/Spacing'
 import Text from '@shared/Text'
+
 import addDelimeter from '@utils/addDelimeter'
+import formatTime from '@utils/formatTime'
 
 // interface임을 명시하기 위해 I라는 prefix붙임
 function Hotel({ hotel }: { hotel: IHotel }) {
+  const [remainedTime, setRemainedTime] = useState(0)
+
+  useEffect(() => {
+    if (hotel.events == null || hotel.events.promoEndTime == null) {
+      return
+    }
+
+    const promoEndTime = hotel.events.promoEndTime
+
+    const timer = setInterval(() => {
+      //parseISO -> string 데이터를 Date 객체로 변환
+      //promoEndTime과 현재시간(new Date)의 차이를 반환
+      const 남은초 = differenceInMilliseconds(
+        parseISO(promoEndTime),
+        new Date('2023-10-17T03:24:00'),
+      )
+
+      if (남은초 < 0) {
+        clearInterval(timer)
+        return
+      }
+
+      setRemainedTime(남은초)
+    }, 1_000)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [hotel.events])
+
+  const tagComponent = () => {
+    if (hotel.events == null) {
+      return null
+    }
+
+    const { name, tagThemeStyle } = hotel.events
+
+    const promotionText =
+      remainedTime > 0 ? ` - ${formatTime(remainedTime)} 남음` : ''
+
+    return (
+      <div>
+        <Tag
+          color={tagThemeStyle.fontColor}
+          backgroundColor={tagThemeStyle.backgroundColor}
+        >
+          {name.concat(promotionText)}
+        </Tag>
+        <Spacing size={8} />
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <ListRow
-        contents={
-          <Flex direction="column">
-            <ListRow.Texts
-              title={hotel.name}
-              subTitle={hotel.comment}
-            ></ListRow.Texts>
-            <Spacing size={4} />
-            <Text typography="t7" color="gray600">
-              {hotel.starRating}성급
-            </Text>
-          </Flex>
-        }
-        right={
-          <Flex direction="column" align="flex-end">
-            <img src={hotel.mainImageUrl} alt="" css={imageStyle} />
-            <Spacing size={8} />
-            <Text bold={true}>{addDelimeter(hotel.price)}원</Text>
-          </Flex>
-        }
-        style={containerStyles}
-      />
-    </div>
+    <Link to={`/hotel/${hotel.id}`}>
+      <div>
+        <ListRow
+          contents={
+            <Flex direction="column">
+              {tagComponent()}
+              <ListRow.Texts
+                title={hotel.name}
+                subTitle={hotel.comment}
+              ></ListRow.Texts>
+              <Spacing size={4} />
+              <Text typography="t7" color="gray600">
+                {hotel.starRating}성급
+              </Text>
+            </Flex>
+          }
+          right={
+            <Flex direction="column" align="flex-end">
+              <img src={hotel.mainImageUrl} alt="" css={imageStyle} />
+              <Spacing size={8} />
+              <Text bold={true}>{addDelimeter(hotel.price)}원</Text>
+            </Flex>
+          }
+          style={containerStyles}
+        />
+      </div>
+    </Link>
   )
 }
 
